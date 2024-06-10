6cc7932476b43f4a11ed011f58358dc542568232
Noah Ross <me@noah.dev>
2024-06-10 06:01:03 -0700

    don't spam new tickets, don't force load, increase timer to 60 seconds

--- src/main/java/rip/plugins/pearlfixer/PearlFixer.java
+++ src/main/java/rip/plugins/pearlfixer/PearlFixer.java
@@ -7,8 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
+import java.util.HashSet;
 
 
 public final class PearlFixer extends JavaPlugin implements Listener {
@@ -16,54 +15,43 @@ public final class PearlFixer extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Bukkit.getPluginManager().registerEvents(this,this);
+        Bukkit.getPluginManager().registerEvents(this, this);
         String requiredSetting = "Paper world setting \"disable-unloaded-chunk-enderpearl-exploit\" must be set tot false\n" +
                 "This warning will always show up, even if the setting is set to false";
         getLogger().warning(requiredSetting);
     }
 
     @EventHandler
-    public void onPearl(EntitySpawnEvent e){
+    public void onPearl(EntitySpawnEvent e) {
 
-        if(!(e.getEntity()instanceof EnderPearl)){
+        if (!(e.getEntity() instanceof EnderPearl)) {
             return;
         }
-        ArrayList<Chunk> chunks = new ArrayList<>();
+        HashSet<Chunk> chunkHashSet = new HashSet<>();
 
         long time = System.currentTimeMillis();
         if (e.getEntity() instanceof EnderPearl) {
-            Bukkit.getScheduler().runTaskTimer(this,(runnable)->{
-
-                e.getEntity().getLocation().getChunk().addPluginChunkTicket(this);
-                e.getEntity().getLocation().getChunk().setForceLoaded(true);
-                chunks.add(e.getEntity().getLocation().getChunk());
+            Bukkit.getScheduler().runTaskTimer(this, (runnable) -> {
 
 
+                boolean hadChunk = chunkHashSet.add(e.getEntity().getLocation().getChunk());
+                if(hadChunk){
+                    e.getEntity().getLocation().getChunk().addPluginChunkTicket(this);
+                }
 
                 if (e.getEntity().isDead()) {
-                    for (Chunk chunk : chunks) {
+                    for (Chunk chunk : chunkHashSet) {
                         chunk.removePluginChunkTicket(this);
-                        chunk.setForceLoaded(false);
                     }
                     runnable.cancel();
-                }else if(e.getEntity()==null){
-                    for (Chunk chunk : chunks) {
+
+                } else if (time + 60000 < System.currentTimeMillis()) {
+                    for (Chunk chunk : chunkHashSet) {
                         chunk.removePluginChunkTicket(this);
-                        chunk.setForceLoaded(false);
-                    }
-                    runnable.cancel();
-                } else if (time + 20000 < System.currentTimeMillis()) {
-                    for (Chunk chunk : chunks) {
-                        chunk.removePluginChunkTicket(this);
-                        chunk.setForceLoaded(false);
                     }
                     runnable.cancel();
                 }
-
-
-            },0,1);
-
-
+            }, 0, 0);
         }
     }
 
